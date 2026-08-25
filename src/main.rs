b149253ff284439b99f0bd3fc26f6caf720da9fc
Jaden Takach <jadentakach@gmail.com>
2026-08-25 00:19:24 -0400

    we are now a tcp server

--- src/main.rs
+++ src/main.rs
@@ -1,86 +1,21 @@
-#[derive(Debug)]
-enum Course {
-    CulinaryArts,
-    AnatomyPhysiology,
-    BusinessOfSports,
-    EnglishWriting,
-    Psychology,
-    Civics,
-    Baking,
-    Precalculus
-}
-
-struct HomeworkEntry {
-    title: String,
-    course: Course,
-    description: String,
-    due_date: String
-}
-
-fn new_entry(title: String, course: Course, description: String, due_date: String) -> HomeworkEntry {
-    HomeworkEntry {
-        title,
-        course,
-        description,
-        due_date
-    }
-}
-
-fn get_input(output: &str) -> String {
-    let mut input: String = String::new();
-    
-    println!("{}", output);
-    std::io::stdin().read_line(&mut input).expect("Failed to read input");
-    return input.trim().to_string();
-}
-
-fn add_homework(entries: &mut Vec<HomeworkEntry>) {
-    let title: String = get_input("Enter the title of the homework:");
-    let course_input: String = get_input("Enter the course by shorthand:\nCulinary Arts: CA\nAnatomy Physiology: AP\nBusiness of Sports: BS\nEnglish Writing: EW\nPsychology: PS\nCivics: CV\nBaking: BK\nPrecalculus: PC");
-    let description: String = get_input("Enter the description of the homework:");
-    let due_date: String = get_input("Enter the due date of the homework:");
-
-    let course: Course = match course_input.as_str() {
-        "CA" => Course::CulinaryArts,
-        "AP" => Course::AnatomyPhysiology,
-        "BS" => Course::BusinessOfSports,
-        "EW" => Course::EnglishWriting,
-        "PS" => Course::Psychology,
-        "CV" => Course::Civics,
-        "BK" => Course::Baking,
-        "PC" => Course::Precalculus,
-
-        _ => {
-            println!("Invalid course entered. Defaulting to CulinaryArts.");
-            Course::CulinaryArts
-        }
-    };
-
-    let entry: HomeworkEntry = new_entry(title, course, description, due_date);
-    entries.push(entry);
-}
-
-fn view_homework(entries: &Vec<HomeworkEntry>) {
-    for entry in entries {
-        println!("Title: {}, Course: {:?}, Description: {}, Due Date: {}", entry.title, entry.course, entry.description, entry.due_date);
-    }
-}
-
-fn exit_loop() {
-    println!("Exiting...");
-}
+use std::net::{TcpListener, TcpStream};
 
 fn main() {
-    let mut homework_entries: Vec<HomeworkEntry> = Vec::new();
+    println!("Preparing to start TCP server\nEnter desired port:");
 
-    loop {
-        let input: String = get_input("Do you want to..\n1. Add a new homework entry\n2. View all homework entries\n3. Exit");
+    let mut port: String = String::new();
+    std::io::stdin().read_line(&mut port).expect("Failed to read port");
 
-        match input.as_str() {
-            "1" => add_homework(&mut homework_entries),
-            "2" => view_homework(&homework_entries),
-            "3" => { exit_loop(); break; },
-            _ => println!("Invalid option selected.")
-        }
+    let port = port.trim();
+
+    let address: String = format!("127.0.0.1:{}", port);
+    let listener: TcpListener = TcpListener::bind(&address).unwrap();
+
+    println!("Server started on port {}", port);
+
+    for stream in listener.incoming() {
+        let unwrapped: TcpStream = stream.unwrap();
+
+        println!("New connection from {}", unwrapped.peer_addr().unwrap().to_string());
     }
 }
