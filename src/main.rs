2174c1126565b99256fc58053bff4b83f1beba21
Jaden Takach <jadentakach@gmail.com>
2026-08-25 00:45:39 -0400

    minimal request handling

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,29 @@
 use std::{
+    fs,
     io::{BufReader, prelude::*},
     net::{TcpListener, TcpStream}
 };
 
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&stream);
-    let http_request: Vec<_> = buf_reader
-        .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    // println!("Incoming Http request: {http_request:#?}"); // cool but clutters the cli
+    if request_line == "GET / HTTP/1.1" {
+        let status: String = String::from("HTTP/1.1 200 OK");
+        let contents: String = fs::read_to_string("index.html").unwrap();
+        let length: usize = contents.len();
 
-    let response = "HTTP/1.1 200 OK\r\n\r\n";
-    stream.write_all(response.as_bytes()).unwrap();
+        let response = format!("{}\r\nContent-Length: {}\r\n\r\n{}", status, length, contents);
+        stream.write_all(response.as_bytes()).unwrap();
+    } else {
+        let status: String = String::from("HTTP/1.1 404 NOT FOUND");
+        let contents: String = fs::read_to_string("404.html").unwrap();
+        let length: usize = contents.len();
+
+        let response = format!("{}\r\nContent-Length: {}\r\n\r\n{}", status, length, contents);
+        stream.write_all(response.as_bytes()).unwrap();
+    }
+    
 }
 
 fn main() {
