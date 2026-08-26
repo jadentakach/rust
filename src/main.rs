b6f6dc0de904c2e500715ee7478506dfff44f1ef
Jaden Takach <jadentakach@gmail.com>
2026-08-25 22:57:03 -0400

    separate elements of polynomial

--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,14 @@ fn main() {
     let mut problem: String = String::new();
     std::io::stdin().read_line(&mut problem).expect("Failed to read input");
     println!("Problem: {}", problem);
+
+    let mut elements: Vec<String> = Vec::new();
+
+    for part in problem.split(" + ") {
+        elements.push(String::from(part));
+    }
+
+    println!("A: {}", elements[0]);
+    println!("B: {}", elements[1]);
+    println!("C: {}", elements[2]);
 }
