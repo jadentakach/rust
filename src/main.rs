c993d0816c3bfb742331088755654c9654e3038e
Jaden Takach <jadentakach@gmail.com>
2026-08-25 22:44:30 -0400

    now making a polynomial solver

--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,6 @@
-use std::{
-    fs,
-    io::{BufReader, BufRead, Write},
-    net::{TcpListener, TcpStream}
-};
-
-fn build_response(status_code: &str, page: &str) -> String {
-    let status: String = format!("HTTP/1.1 {}", status_code);
-    let contents: String = fs::read_to_string(page).unwrap();
-    let length: usize = contents.len();
-
-    let response: String = format!("{}\r\nContent-Length: {}\r\n\r\n{}", status, length, contents);
-    
-    response
-}
-
-fn handle_connection(mut stream: TcpStream) {
-    let buf_reader = BufReader::new(&stream);
-    let request_line = buf_reader.lines().next().unwrap().unwrap();
-
-    if request_line == "GET / HTTP/1.1" {
-        let response = build_response("200 OK", "pages/index.html");
-        stream.write_all(response.as_bytes()).unwrap();
-    } else {
-        let response = build_response("404 NOT FOUND", "pages/404.html");
-        stream.write_all(response.as_bytes()).unwrap();
-    }
-    
-}
-
-fn get_input(prompt: &str) -> String {
-    println!("{}", prompt);
-    let mut input: String = String::new();
-
-    std::io::stdin().read_line(&mut input).expect("Failed to read input");
-    let input = input.trim();
-
-    return input.to_string();
-}
-
 fn main() {
-    println!("Preparing to start TCP server");
-
-    let port: String = get_input("Enter port: ");
-
-    let address: String = format!("127.0.0.1:{}", port);
-    let listener: TcpListener = TcpListener::bind(&address).unwrap();
-
-    println!("Server started on port {}", port);
-
-    for stream in listener.incoming() {
-        let unwrapped: TcpStream = stream.unwrap();
-
-        println!("New connection from {}", unwrapped.peer_addr().unwrap().to_string());
-        handle_connection(unwrapped);
-    }
+    println!("Input problem:");
+    let mut problem: String = String::new();
+    std::io::stdin().read_line(&mut problem).expect("Failed to read input");
+    println!("Problem: {}", problem);
 }
