5adfdac9c6261ff15e6be86a50021a27970469ef
Jaden Takach <jadentakach@gmail.com>
2026-08-25 23:26:11 -0400

    factoring numbers

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,33 @@
-fn main() {
-    println!("Input problem:");
-    let mut problem: String = String::new();
-    std::io::stdin().read_line(&mut problem).expect("Failed to read input");
-    println!("Problem: {}", problem);
+fn get_input(prompt: &str) -> String {
+    let mut input: String = String::new();
+    println!("{}", prompt);
 
-    let mut elements: Vec<String> = Vec::new();
+    std::io::stdin().read_line(&mut input).expect("Failed to read input");
 
-    for part in problem.split(" + ") {
-        elements.push(String::from(part));
+    input
+}
+
+fn factors(int: i32) -> Vec<Vec<i32>> {
+    let mut listed_factors: Vec<Vec<i32>> = Vec::new();
+    for i in 1..=int {
+        for j in 1..=int {
+            if i * j == int && !listed_factors.contains(&(vec![i, j])) && !listed_factors.contains(&(vec![j, i])) {
+                listed_factors.push(vec![i, j]);
+            }
+        }
     }
 
-    println!("A: {}", elements[0]);
-    println!("B: {}", elements[1]);
-    println!("C: {}", elements[2]);
+    return listed_factors;
+}
+
+fn main() {
+    let mut input: String = get_input("Input number: ");
+    let int: i32 = input.trim().parse::<i32>().unwrap();
+    input.clear();
+
+    println!("Factors of {}:", int);
+    let factors: Vec<Vec<i32>> = factors(int);
+    for factor in factors {
+        println!("[{}, {}]", factor[0], factor[1]);
+    }
 }
